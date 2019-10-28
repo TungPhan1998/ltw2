4856a5eae9a1e2ff2fcc93061b6f160c71753746
Student <Student@tdc.edu.vn>
2019-10-28 16:23:24 +0700

    ltw2 20 thang 10
    hehe

---
+++ blog/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/lienhe', function () {
+    return view('lienhe');
+});
+Route::get('/trangchu', function () {
+    return view('trangchu');
+});
+Route::get('/ban', function () {
+    return view('ban');
+});
+Route::get('/ghe', function () {
+    return view('ghe');
+});
+Route::get('/sanpham', function () {
+    return view('sanpham');
+});
+Route::get('/gioithieu', function () {
+    return view('gioithieu');
+});
+
